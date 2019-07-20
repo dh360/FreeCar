55cbda99f69854ffebd2339fea2e68814e9b715c
ding hao <dinghao360@163.com>
2019-07-20 17:19:11 +0800

    add   method  move  and  change  rotation

--- assets/scripts/JoyStick.js
+++ assets/scripts/JoyStick.js
@@ -22,22 +22,24 @@ cc.Class({
 
     onLoad() {
         //挂载监听事件
-        console.log('挂载 主场景');
+        // console.log('挂载 主场景');
 
         this.radius = this.ring.width / 2; // 半径
         let center = this.ring.getAnchorPoint(); //ring节点圆心
-        let centerX = this.ring.anchorX * this.ring.width;
-        let centerY = this.ring.anchorY * this.ring.height;
+        this.centerX = this.ring.anchorX * this.ring.width;
+        this.centerY = this.ring.anchorY * this.ring.height;
         this.initListenerEvent();
     },
 
     //
     touchStartHandler(event) {
+        console.log('开始触摸');
         const touchPos = this.node.convertToNodeSpaceAR(event.getLocation());
 
         let figerPosition = event.getLocation(); // 获取鼠标/手指触摸位置
-        console.log('手指触摸位置', figerPosition);
+        // console.log('手指触摸位置', figerPosition);
         const distance = touchPos.sub(this.ring.getPosition()).mag();
+        this._stickPos = this.ring.getPosition();
 
         if (distance <= this.radius) {
             this.dot.setPosition(touchPos);
@@ -45,19 +47,45 @@ cc.Class({
     },
 
     touchMoveHandler(event) {
+        console.log('触摸过程中')
         const touchPos = this.node.convertToNodeSpaceAR(event.getLocation());
+        const distance = touchPos.mag();
+        const posX = this._stickPos.x + touchPos.x;
+        const posY = this._stickPos.y + touchPos.y;
+        // console.log(touchPos);
+        const p = cc.v2(posX, posY).sub(this.ring.getPosition()).normalize();
+        if (distance <= this.radius) {
+            this.dot.setPosition(touchPos);
+        } else {
+            const x = this._stickPos.x + p.x * this.radius;
+            const y = this._stickPos.y + p.y * this.radius;
+            this.dot.setPosition(cc.v2(x, y));
 
+        }
+        // console.log(p)
+
+        //移动车
+        const car = this.node;
+        let carPos = this.player.getPosition();
+        this.player.rotation = 90 - cc.misc.radiansToDegrees(
+            Math.atan2(p.y, p.x)
+        );
+
+        let newPos = this.player.position.add(p.mul(3));
+        this.player.setPosition(newPos);
     },
 
     touchEndHandler(event) {
-
+        console.log('触摸结束')
+        // dot  归位
+        this.dot.setPosition(this.centerX, this.centerY);
 
     },
 
 
     initListenerEvent() {
         this.node.on(cc.Node.EventType.TOUCH_START, this.touchStartHandler, this);
-        this.node.on(cc.Node.EventType.TOUCH_MOVE, this.touchEndHandler, this);
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, this.touchMoveHandler, this);
         this.node.on(cc.Node.EventType.TOUCH_END, this.touchEndEvent, this);
         this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.touchEndEvent, this);
     }
--- library/imports/a8/a8c89a71-7d6b-454b-93c2-f74f36849cca.js
+++ library/imports/a8/a8c89a71-7d6b-454b-93c2-f74f36849cca.js
@@ -28,32 +28,68 @@ cc.Class({
 
     onLoad: function onLoad() {
         //挂载监听事件
-        console.log('挂载 主场景');
+        // console.log('挂载 主场景');
 
         this.radius = this.ring.width / 2; // 半径
         var center = this.ring.getAnchorPoint(); //ring节点圆心
-        var centerX = this.ring.anchorX * this.ring.width;
-        var centerY = this.ring.anchorY * this.ring.height;
+        this.centerX = this.ring.anchorX * this.ring.width;
+        this.centerY = this.ring.anchorY * this.ring.height;
         this.initListenerEvent();
     },
 
 
     //
     touchStartHandler: function touchStartHandler(event) {
+        console.log('开始触摸');
         var touchPos = this.node.convertToNodeSpaceAR(event.getLocation());
 
         var figerPosition = event.getLocation(); // 获取鼠标/手指触摸位置
-        console.log('手指触摸位置', figerPosition);
+        // console.log('手指触摸位置', figerPosition);
         var distance = touchPos.sub(this.ring.getPosition()).mag();
+        this._stickPos = this.ring.getPosition();
 
         if (distance <= this.radius) {
             this.dot.setPosition(touchPos);
         }
     },
-    touchEndHandler: function touchEndHandler(event) {},
+    touchMoveHandler: function touchMoveHandler(event) {
+        console.log('触摸过程中');
+        var touchPos = this.node.convertToNodeSpaceAR(event.getLocation());
+        var distance = touchPos.mag();
+        var posX = this._stickPos.x + touchPos.x;
+        var posY = this._stickPos.y + touchPos.y;
+        // console.log(touchPos);
+        var p = cc.v2(posX, posY).sub(this.ring.getPosition()).normalize();
+        if (distance <= this.radius) {
+            this.dot.setPosition(touchPos);
+        } else {
+            var x = this._stickPos.x + p.x * this.radius;
+            var y = this._stickPos.y + p.y * this.radius;
+            this.dot.setPosition(cc.v2(x, y));
+        }
+        // console.log(p)
+
+        //移动车
+        var car = this.node;
+        var carPos = this.player.getPosition();
+        // car.setPosition(carPos.x + 1, carPos.y);
+        // console.log(carPos);
+        // car.rotation = carPos.x;
+        // car.rotationY = carPos.y;
+        // car.rotation = carPos.y / carPos.x;
+        this.player.rotation = 90 - cc.misc.radiansToDegrees(Math.atan2(p.y, p.x));
+
+        var newPos = this.player.position.add(p.mul(3));
+        this.player.setPosition(newPos);
+    },
+    touchEndHandler: function touchEndHandler(event) {
+        console.log('触摸结束');
+        // dot  归位
+        this.dot.setPosition(this.centerX, this.centerY);
+    },
     initListenerEvent: function initListenerEvent() {
         this.node.on(cc.Node.EventType.TOUCH_START, this.touchStartHandler, this);
-        this.node.on(cc.Node.EventType.TOUCH_MOVE, this.touchEndHandler, this);
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, this.touchMoveHandler, this);
         this.node.on(cc.Node.EventType.TOUCH_END, this.touchEndEvent, this);
         this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.touchEndEvent, this);
     }
